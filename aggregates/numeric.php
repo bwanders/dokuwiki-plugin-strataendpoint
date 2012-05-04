44de11178477b612cbde657108f994c039bcd67d
Brend Wanders <b.wanders@utwente.nl>
2012-05-04 17:01:43 +0200

    Improves explanation of strictly numerical values.

--- aggregates/numeric.php
+++ aggregates/numeric.php
@@ -27,7 +27,7 @@ class plugin_strata_aggregate_numeric extends plugin_strata_aggregate {
 
     function getInfo() {
         return array(
-            'desc'=>'Converts all numerical values to numbers. This is only necessary when exporting data through an endpoint. Any value not starting with a number will become 0. If the \'strict\' hint is used, values that are not strictly numeric are left intact.',
+            'desc'=>'Converts all numerical values to numbers. This is only necessary when exporting data through an endpoint. Any item that does not have a clear numeric value (i.e. starts with a number) is counted as 0. If the \'strict\' hint is used, values that are not strictly numeric (i.e. contains only a number) are left intact.',
             'hint'=>'\'strict\' to leave non-numeric values intact',
             'tags'=>array('numeric')
         );
